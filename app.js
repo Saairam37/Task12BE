46bbbce0449b2d6381ae27f41db2a93b858cb541
Saairam37 <saairam185@gmail.com>
2025-08-12 03:19:53 +0530

    1st commit

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const errorRoute = require('./utils/errorRoute');
+const logger = require('./utils/logger');
+const authRouter = require('./routes/authRouter');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+
+
+const app = express();
+app.use(cors({
+  origin: ['http://localhost:5173', 'https://password-reset-flow-gb7g.onrender.com', 'https://passwordreset-flow-frontend.netlify.app'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  credentials: true
+}));
+
+
+
+app.use(express.json());
+
+app.use(cookieParser());
+
+app.use(logger);
+
+app.use("/api/v1/auth", authRouter);
+
+
+app.use(errorRoute);
+
+module.exports = app;
